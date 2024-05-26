538c929a1a1d82505880dbcf89c5a14ddfbcc490
siddharthkumarrrrr <123976905+siddharthkumarrrrr@users.noreply.github.com>
2024-05-27 00:08:01 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -43,16 +43,61 @@ res.json({msg:"course added successfully"});
 
 });
 
-router.post("/purchaseccourses",async(req,res)=>{
-    const user=await user.findOne({
-       usernaem: req.headers.username
-    });
+router.post("/signup", async (req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+  
+      const userexist=user.findOne({username,password});
+    if(userexist){
+        res.status(500).json({ msg:"user is singuped already"});
+    }
 
-    const courses=await course.find({
-      _id:{
-        "$in":user.purchasedCourse
+    try{
+    const newuser=await user.create({
+        username,
+        password
+    });
+   res.json({msg:"new user is created successfully"});}
+ catch(err){
+    res.status(400).json({error:"Error in creation of new user"});
+ }
+});
+
+router.post("/courses/:courseid",usermiddleware,async(req,res)=>{
+   const courseid=req.params.courseid;
+   const username=req.headers.username;
+   await user.updateOne({
+    username
+   },
+  {
+  "$push":{
+    purchasedCourse:courseid
+  }
+  }
+)
+res.json({msg:"course added successfully"});
+  
+
+});
+
+router.get("/purchasedcourses",  async (req, res) => {
+  
+    const username = req.headers.username;
+
+    const user1 = await user.findOne({ username });
+
+    if (!user1) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const courses = await course.find({
+      _id: {
+        "$in": user1.purchasedCourse
       }
     });
+
+    res.status(200).json(courses);
+
 });
 
 module.exports=router;
