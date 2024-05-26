11bad66c35e54f6b10e9434fababc1759de257f0
siddharthkumarrrrr <123976905+siddharthkumarrrrr@users.noreply.github.com>
2024-05-27 00:06:53 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 
 async function connectToDatabase() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/course', {
+       await mongoose.connect('mongodb+srv://siddharthkumarrrrr:Sidd123@cluster0.7rhydih.mongodb.net/sid', {
     useNewUrlParser: true,
             useUnifiedTopology: true
         });
